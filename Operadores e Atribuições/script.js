4cc623fb88a0bc541a350ca2cef61d89ae7f2a59
Eduardo Alencar <alencareduardo17@gmail.com>
2024-01-04 22:35:41 -0300

    Adicionando os Atribuições e Ternario

---
+++ Operadores e Atribuições/script.js
@@ -0,0 +1,41 @@
+var x = 5;
+var y = 10;
+
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+var idade = 21;
+var naoPossuiDiabetes = false;
+
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'Não pode beber';
+
+console.log(podeBeber); // false
+
+
+var possuiFaculdade = true;
+if (possuiFaculdade)
+    console.log("Possui sim");
+else
+    console.log("Não possui");
+
+
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+scroll += 500;
+console.log(scroll);
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+darCredito = (possuiCarro && possuiCasa) ? 'Credito aprovado' : 'Credito negado';
+console.log(darCredito);
